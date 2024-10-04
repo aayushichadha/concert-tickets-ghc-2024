ae26d14153d76f706c2da3eaa00315e62680b547
Khushboo Ahuja <khushboo.ahuja@uber.com>
2024-10-04 16:36:16 -0400

    Adding mapping step for ticket type

---
+++ ticket-registry/mappers/tickets.go
@@ -0,0 +1,35 @@
+package mappers
+
+import (
+	"math/rand"
+	"ticket-registry/models"
+)
+
+func AdaptToTicketListFormat(getTicketsRequest *models.GetTicketsRequest) (response *[]models.Ticket, err error) {
+	var resp []models.Ticket
+	for i := 0; i < getTicketsRequest.Quantity; i++ {
+		ticket := models.Ticket{
+			TicketID:   rand.Intn(100),
+			TicketType: getTicketsRequest.TicketType,
+		}
+		resp = append(resp, ticket)
+	}
+	return &resp, nil
+}
+
+func AdaptToTicketTypeKey(ticketType string) models.TicketType {
+	switch ticketType {
+	case "vip-front-row":
+		return models.VIPFrontRow
+	case "platinum-seating":
+		return models.PlatinumSeating
+	case "general-admission":
+		return models.GeneralAdmission
+	case "balcony-seating":
+		return models.BalconySeating
+	case "superfan-pit":
+		return models.SuperfanPit
+	default:
+		return ""
+	}
+}
